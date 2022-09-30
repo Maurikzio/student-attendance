367c7020ac2018e2e4e4753b70df0e4c197de007
Mauricio.M <m.morocho1196@gmail.com>
2022-10-01 00:25:13 +0300

    Added logic to check if the token is valid

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
+import { onAuthStateChanged } from "firebase/auth";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { Routes, Route } from "react-router-dom";
 import Home from "./components/Home";
 import Login from "./components/Login";
 import ProtectedRoute from "./components/ProtectedRoute";
 import Register from "./components/Register";
+import { auth } from "./firebase/firebaseConfig";
+import { logoutUser } from "./store/user/userSlice";
 
 export default function App() {
+  const { userId, userToken } = useSelector((state) => state.user);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const unsub = onAuthStateChanged(auth, (currentUser) => {
+      if(userId === currentUser?.uid && userToken !== currentUser?.accessToken) {
+        dispatch(logoutUser());
+      }
+    })
+
+    return () => unsub()
+  }, [])
+
   return (
     <div className="bg-slate-300 h-screen text-black flex">
       {/* <AuthProvider>
@@ -18,7 +36,6 @@ export default function App() {
           <Route path="/register" element={<Register/>} />
         </Routes>
       </AuthProvider> */}
-
         <Routes>
           <Route path="/" element={
             <ProtectedRoute>
