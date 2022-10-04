3a7bf05455ab0aa896ab5e808168a2fb561f166c
Mauricio.M <m.morocho1196@gmail.com>
2022-10-04 08:16:00 +0300

    Fixed issue when option is selected the styles are not applied

--- src/components/Select.js
+++ src/components/Select.js
@@ -24,7 +24,7 @@ const Select = ({ options, label, onChange }) => {
   return (
     <div className="top-16 w-full">
       {label ? <div className="text-sm font-medium text-gray-700 mb-1">{label}</div> : null }
-      <Combobox value={selectedOption} onChange={handleOnChange}>
+      <Combobox value={selectedOption} by="id" onChange={handleOnChange}>
         <div className="relative">
           {/* <div className="relative w-full cursor-default overflow-hidden rounded-md bg-white text-left shadow-md focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 focus-visible:ring-offset-2 focus-visible:ring-offset-teal-300 sm:text-sm"> */}
           <div className="relative w-full cursor-default bg-white text-left shadow-md sm:text-sm rounded-md">
