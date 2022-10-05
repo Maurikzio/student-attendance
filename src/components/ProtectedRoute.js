3d6e06062b148da9dc5e10d38b2576de0be52648
Mauricio.M <m.morocho1196@gmail.com>
2022-10-05 06:04:35 +0300

    Added logic to check if the token in ls is the same as from onAuthStateChanged

--- src/components/ProtectedRoute.js
+++ src/components/ProtectedRoute.js
@@ -1,9 +1,27 @@
-import { useSelector } from "react-redux";
+import { onAuthStateChanged } from "firebase/auth";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { Navigate } from "react-router-dom";
+import { auth } from "../firebase/firebaseConfig";
+import { logoutUser } from "../store/user/userSlice";
 import Layout from "./Layout";
 
 const ProtectedRoute = ({ children}) => {
   const currUser =  useSelector(state => state.user);
+  const dispatch = useDispatch();
+
+  const { userToken:uToken = null } = localStorage.getItem('ea') ? JSON.parse(localStorage.getItem('ea')) : {};
+
+  /* To check if the token in local is the same as the one from  onAuthStateChanged */
+ useEffect(() => {
+    const unsub = onAuthStateChanged(auth, (currentUser) => {
+      if(currentUser?.accessToken !== uToken){
+        dispatch(logoutUser());
+      }
+    })
+    return () => unsub()
+  }, [])
+
 
   if (!currUser.userId && !currUser.userToken) return <Navigate to="/ingresar" />
 
